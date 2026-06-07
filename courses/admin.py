de5f826e8c290b9e8e252a415ee9377627a59976
Christopher Jimmy <christopherjimmy999@gmail.com>
2026-06-06 20:37:45 -0400

    added Chapter to course admin panel

--- courses/admin.py
+++ courses/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from .models import Chapter
 
 from .models import Course #Import my User model (Teacher/Student)
 admin.site.register(Course) #Registers my User model to the admin page so I can add teachers and students through the admin interface
+admin.site.register(Chapter) #Registers my User model to the admin page 
