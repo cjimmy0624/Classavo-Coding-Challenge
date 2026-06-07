718ff0f0380902240068d4bf8264471e66717647
Christopher Jimmy <christopherjimmy999@gmail.com>
2026-06-06 20:24:49 -0400

    added Courses to Admin Panel

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Course #Import my User model (Teacher/Student)
+admin.site.register(Course) #Registers my User model to the admin page so I can add teachers and students through the admin interface
