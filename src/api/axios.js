ec6329fa4039b343dd14d1d317d5face882176fd
Christopher Jimmy <christopherjimmy999@gmail.com>
2026-06-08 15:41:16 -0400

    trying to fix role undefined

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,11 +3,23 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: 'http://localhost:8000/api',
 });
+
 api.interceptors.request.use(config => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers['Authorization'] = `Bearer ${token}`;
+    const token = localStorage.getItem('accessToken');
+
+    const publicRoutes = [
+        '/users/register/',
+        '/users/login/'
+    ];
+
+    const isPublicRoute = publicRoutes.some(route =>
+        config.url.includes(route)
+    );
+
+    if (token && !isPublicRoute) {
+        config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
 });
 
--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import UserRegistrationView
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import UserRegistrationView, CustomTokenObtainPairView
 
 urlpatterns = [
-    path('register/', UserRegistrationView.as_view(), name='register'), #User registration URL
-    path('login/', TokenObtainPairView.as_view(), name='token_access'), #User login URL
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), #Refreshing JWT tokens URL
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
