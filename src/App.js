bdebf332225082ce950f43947a7e79e093a9a56d
Christopher Jimmy <christopherjimmy999@gmail.com>
2026-06-08 18:25:24 -0400

    front end send pages to correct spot

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import { Routes, Route } from "react-router-dom";
-
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import CourseList from "./pages/CourseList";
-import CourseDetail from "./pages/CourseDetail";
-import ChapterDetail from "./pages/ChapterDetail";
+import InstructorPage from "./pages/InstructorPage";
+import StudentPage from "./pages/StudentPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      {/* default page */}
+      <Route path="/" element={<Navigate to="/login" />} />
+
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
 
-      <Route path="/courses" element={<CourseList />} />
-      <Route path="/courses/:id" element={<CourseDetail />} />
-
-      <Route path="/chapters/:id" element={<ChapterDetail />} />
-
       <Route path="/instructor" element={<InstructorPage />} />
-      <Route path="/student" element={<StudentPage />} /> 
+      <Route path="/student" element={<StudentPage />} />
     </Routes>
   );
 }
