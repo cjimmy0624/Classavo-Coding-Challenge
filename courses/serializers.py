eb9c4f489c2bf6f809a583616e5157914f805a1a
Christopher Jimmy <christopherjimmy999@gmail.com>
2026-06-08 20:23:41 -0400

    fixed save chapter button

--- courses/serializers.py
+++ courses/serializers.py
@@ -4,17 +4,17 @@ from .models import Course, Chapter, Enrollment
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
-        fields = ['id', 'title', 'description', 'instructor', 'creationTime']
+        fields = ['id','title','description','instructor','creationTime']
         read_only_fields = ['instructor', 'creationTime']
 
 class ChapterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Chapter
-        fields = ['id','title','content','publicOrPrivate','order','course']
-        read_only_fields = ['course']  # course will be assigned in request or view
+        fields = ['id','title','content','publicOrPrivate','order']
+        read_only_fields = ['order']
 
 class EnrollmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Enrollment
-        fields = ['id', 'student', 'course', 'enrollmentTime']
+        fields = ['id','student','course','enrollmentTime']
         read_only_fields = ['student', 'course', 'enrollmentTime']
