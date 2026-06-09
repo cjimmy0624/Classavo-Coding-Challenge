921ad835a5e343f0bc29670b450af45f6b5f265c
Christopher Jimmy <christopherjimmy999@gmail.com>
2026-06-08 20:15:57 -0400

    updated manage chapters

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Register from "./pages/Register";
 import InstructorDashboard from "./pages/instructor/InstructorDashboard";
 import CreateCourse from "./pages/instructor/CreateCourse";
 import CourseDetail from "./pages/instructor/CourseDetail";
+
 import CreateChapter from "./pages/instructor/CreateChapter";
 import ManageChapters from "./pages/instructor/ManageChapters";
-import CreateChapter from "./pages/instructor/CreateChapter";
 
 import StudentPage from "./pages/StudentPage";
 
@@ -26,8 +26,10 @@ function App() {
       <Route path="/instructor" element={<InstructorDashboard />} />
       <Route path="/create-course" element={<CreateCourse />} />
       <Route path="/course/:id" element={<CourseDetail />} />
+
+      {/* FIXED - only ONE create-chapter route */}
       <Route path="/course/:id/create-chapter" element={<CreateChapter />} />
-      <Route path="/course/:id/create-chapter" element={<CreateChapter />} />
+
       <Route path="/course/:id/manage-chapters" element={<ManageChapters />} />
 
       {/* student */}
