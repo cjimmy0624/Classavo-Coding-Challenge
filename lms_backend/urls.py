cde3f77ee04bc05bc3804a080d3b351875954367
Christopher Jimmy <christopherjimmy999@gmail.com>
2026-06-15 13:25:31 -0400

    fixes

--- lms_backend/urls.py
+++ lms_backend/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from users.views import ask_ai
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/users/', include('users.urls')),
-
     path('api/', include('courses.urls')),
-
     path("users/", include("users.urls")),
+    path ('api/ai/ask', ask_ai)
 ]
